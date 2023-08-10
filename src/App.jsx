0befb18290f2c6182a9a886d901ae442b6aebe17
danny-mak1993 <danny.mak2@gmail.com>
2023-08-09 22:43:29 -0500

    added css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  
+export default function App() {
+  return "Hi"
 }
-
-export default App
