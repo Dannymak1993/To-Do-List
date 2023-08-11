34a2e10a46b9299c27420ed636943e09134540ed
danny-mak1993 <danny.mak2@gmail.com>
2023-08-11 12:34:35 -0500

    passed toggle and delete todos as props into app.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,14 @@
-export function TodoList({ todos }) {
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
       {todos.length === 0 && "No Todos"}
       {todos.map((todo) => {
         return (
           <TodoItem
-            id={todo.id}
-            completed={todo.completed}
-            title={todo.title}
+            {...todo}
             key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
           />
         );
       })}
